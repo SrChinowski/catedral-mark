1265f70891fe5781c27d013327264102b5e1f9a0
Carlos Leon <cleonve@icloud.com>
2022-05-12 20:50:24 -0500

    Add app code from superprops

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react';
+import Sticky from 'react-stickynode';
 import { ThemeProvider } from 'styled-components';
 import ResetCSS from './assets/css/style';
+import Navbar from './components/common/Navbar';
 import GlobalStyle, { AppWrapper } from './components/containers/containers';
 import { theme } from './utils/constants/common';
 
