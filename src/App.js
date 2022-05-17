d8be66264beccc7bebbb25deafd3c24881088235
Andres Calderon <andrescalderon015@gmail.com>
2022-05-16 20:23:51 -0500

    Error

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ import Experiences from './components/common/Experiences';
 import FeatureSlider from './components/common/FeatureSlider';
 import Navbar from './components/common/Navbar';
 import Pricing from './components/common/Pricing';
+import CallToAction from './components/common/CallToAction';
+import ChooseUs from './components/common/ChooseUs';
+import Faq from './components/common/Faq';
+import Footer from './components/common/Footer';
+import KeyFeatures from './components/common/KeyFeatures';
 import TestimonialSection from './components/common/Testimonials';
 import GlobalStyle, { AppWrapper } from './components/containers/containers';
 import { theme } from './utils/constants/common';
@@ -31,6 +36,11 @@ function App() {
             <FeatureSlider />
             <TestimonialSection />
             <AvailableStore />
+            <CallToAction />
+            <KeyFeatures />
+            <ChooseUs />
+            <Faq />
+            <Footer />
 
           </ContentWrapper>
           {/* <Sticky top={0} innerZ={9999} activeClass="sticky-active">
