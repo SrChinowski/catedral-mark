ed2d2a1651405fb5f5813801248effa9daa89d14
Carlos Leon <cleonve@icloud.com>
2022-05-30 21:04:13 -0500

    Bug data with hook

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -17,6 +17,7 @@ import {
 import { ThemeProvider } from 'styled-components';
 import { useState } from 'react';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { Controller, useForm } from 'react-hook-form';
 
 export const themeOptions = createTheme({
   palette: {
@@ -38,14 +39,24 @@ export const themeOptions = createTheme({
 
 const AppLogin = () => {
 
+    //Data States
     const [values, setValues] = useState({
         mail: '',
         password: '',
         showPassword: false,
     });
 
+    //UI States
+    const { control, handleSubmit } = useForm({
+        defaultValues: {
+          mail: '',
+          pswd: ''
+        }
+    });
+
+    //Helpers
     const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });
+        setValues({ ...values, [prop]: event.target.value });
     };
 
     const handleClickShowPassword = () => {
@@ -55,56 +66,74 @@ const AppLogin = () => {
     });
     };
 
+    //Fetchers
+    const onSubmit = data => console.log(data);
+
     return ( 
         <ThemeProvider theme={themeOptions}>
             <LoginWrapper>
             <GlobalStyle />
             <LoginLayout>
                 <Card >
-                    <Heading content={'Hola'} style={{fontWeight: '700', marginBottom: '5px'}}/>
-                    <Text content={'Es bueno verte de nuevo 👋'} style={{color: 'rgba(14, 14, 23, 0.7)'}}/>
-                    <Separator/>
-                    <TextField 
-                        label="Correo" 
-                        variant="outlined" fullWidth 
-                        type={'email'} size='medium'
-                        onChange={handleChange('mail')}
-                        value={values.mail}
-                    /> 
-                    <Separator/>
-                    <FormControl variant="outlined" fullWidth>
-                        <InputLabel htmlFor="outlined-adornment-password">Contraseña</InputLabel>
-                        <OutlinedInput
-                            size='medium'
-                            type={values.showPassword ? 'text' : 'password'}
-                            value={values.password}
-                            onChange={handleChange('password')}
-                            endAdornment={
-                                <InputAdornment position="end">
-                                    <IconButton
-                                        aria-label="toggle password visibility"
-                                        onClick={handleClickShowPassword}
-                                        edge="end"
-                                    >
-                                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                                    </IconButton>
-                                </InputAdornment>
-                            }
-                            label="Contraseña"
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <Heading content={'Hola'} style={{fontWeight: '700', marginBottom: '5px'}}/>
+                        <Text content={'Es bueno verte de nuevo 👋'} style={{color: 'rgba(14, 14, 23, 0.7)'}}/>
+                        <Separator/>
+                        <Controller
+                            name="mail"
+                            control={control}
+                            render={({...props}) => (
+                                <TextField 
+                                    {...props}
+                                    label="Correo" 
+                                    variant="outlined" fullWidth 
+                                    type={'email'} size='medium'
+                                    onChange={ handleChange('mail')}
+                                    value={values.mail}
+                                /> 
+                            )}
                         />
-                    </FormControl>
-                    <Separator/>
-                    <div style={{display: 'flex', justifyContent: 'space-between', marginTop: '15px'}}>
-                        <FormControlLabel control={<Checkbox defaultChecked />} label="Recuérdame" />
-                        <Button variant="text">Olvidé la contraseña </Button>
-                    </div>
-                    <Separator/>
-                    <Button variant="contained" color='primary' size='large' style={{width: '90%', alignSelf: 'center'}}>Iniciar Sesión</Button>
-                    <Separator/>
-                    <div  style={{alignSelf: 'center', display: 'flex'}}>
-                        <Text content={'¿No tienes una cuenta?'} style={{color: 'rgba(14, 14, 23, 0.7)'}}/>
-                        <b style={{color: themeOptions.palette.primary.main, cursor: 'pointer', marginLeft: '5px'}}>Registrate</b>
-                    </div>
+                        <Separator/>
+                        <Controller
+                            name="pswd"
+                            control={control}
+                            render={() => 
+                                <FormControl variant="outlined" fullWidth>
+                                    <InputLabel htmlFor="outlined-adornment-password">Contraseña</InputLabel>
+                                    <OutlinedInput
+                                        size='medium'
+                                        type={values.showPassword ? 'text' : 'password'}
+                                        value={values.password}
+                                        onChange={handleChange('password')}
+                                        endAdornment={
+                                            <InputAdornment position="end">
+                                                <IconButton
+                                                    aria-label="toggle password visibility"
+                                                    onClick={handleClickShowPassword}
+                                                    edge="end"
+                                                >
+                                                {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                                </IconButton>
+                                            </InputAdornment>
+                                        }
+                                        label="Contraseña"
+                                    />
+                                </FormControl>
+                            }
+                        />
+                        <Separator/>
+                        <div style={{display: 'flex', justifyContent: 'space-between', marginTop: '15px'}}>
+                            <FormControlLabel control={<Checkbox defaultChecked />} label="Recuérdame" />
+                            <Button variant="text">Olvidé la contraseña </Button>
+                        </div>
+                        <Separator/>
+                        <Button variant="contained" color='primary' size='large' style={{width: '90%', alignSelf: 'center'}} type="submit">Iniciar Sesión</Button>
+                        <Separator/>
+                        <div  style={{alignSelf: 'center', display: 'flex'}}>
+                            <Text content={'¿No tienes una cuenta?'} style={{color: 'rgba(14, 14, 23, 0.7)'}}/>
+                            <b style={{color: themeOptions.palette.primary.main, cursor: 'pointer', marginLeft: '5px'}}>Registrate</b>
+                        </div>
+                    </form>
                 </Card>
             </LoginLayout>
         </LoginWrapper>
