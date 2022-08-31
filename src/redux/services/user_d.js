a914b8203b83ab84ba4a54ac674a7af20ddab3f1
Andres Calderon <andrescalderon015@gmail.com>
2022-08-31 15:28:25 -0500

    add services 4 crud users 2.0

--- src/redux/services/user_d.js
+++ src/redux/services/user_d.js
@@ -2,3 +2,15 @@ import api from "./api";
 
 export const getAllusersService = () => api.get('users/all')
 .then(({data}) => data);
+
+export const deleteUsersService = (id) => api.get(`users/${id}/delete`)
+.then(({data}) => data);
+
+export const updateUserService = (id) => api.get(`users/${id}/update`)
+.then(({data}) => data);
+
+export const disableUserdervice = (id) => api.get(`users/${id}/disable`)
+.then(({data}) => data);
+
+export const userInfoService = (id) => api.get(`user/${id}/info`)
+.then(({data}) => data);
